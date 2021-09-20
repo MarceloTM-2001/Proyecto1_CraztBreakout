89ea2b1e925b369c1665a7bf87a9031d31801140
Marcelo Truque <machotm@hotmail.com>
2021-09-20 15:10:55 -0600

    Correcciones clase bola, modificación, pasó a ser un array

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ using namespace std;
  */
 class Bloque{
 private:
-    string Sorpresa;//Tipo de sorpresa
-    int destroyed(){//Se destruyó el bloque
+    string Sorpresa; //Tipo de sorpresa
+    int destroyed(){ //Se destruyó el bloque
         tipo="Destruido";
         return this->puntos;
     }
@@ -19,6 +19,7 @@ public:
     int vida;
     int puntos;
     string tipo;
+
     string getSorpresa(){
         return this->Sorpresa;
     }
@@ -27,10 +28,17 @@ public:
         this->Sorpresa=Sorpresa;
     }
 
+    /**
+     * Define si el bloque contiene o no sorpresa
+     * La probabilidad de tener sorpresa es del 25%
+     * La probabilidad de una sorpresa 25%
+     * La probabilidad de todas las sorpresas es la misma 25%,
+     * Por ende la probabilidad de que ub bloque contenga una sorpresa en específico es de 6.25%
+     */
     void virtual defineSorpresa(){
         int probability=rand()%4+1;//Genera un número del 1-4,
         if(probability==1){ //Probabilidad del 25%
-            int probability2=rand()%4+1;
+            int probability2=rand()%4+1;//Genera un número de 1-4
             if (probability2==1){
                 this->Sorpresa="+V";//Aumentar velocidad de la bola
             }else if (probability2==2){
@@ -45,16 +53,28 @@ public:
         }
     }
 
+    /**
+     * Un bloque recibió un golpe de una bola
+     *
+     * @return La cantidad de puntos que se deben sumar por el golpe
+     * No se suman si el bloque no se destruyó
+     */
     int virtual hit(){
-        this->vida-=1;
+        this->vida--;
         if (this->vida==0){
-            return destroyed();
+            return destroyed();//Puntos a sumar
         }else{
-            return 0;
+            return 0;//Puntos a sumar
         }
     }
+
 };
 
+/**
+ * Bloque Común
+ * Da 10 puntos
+ * 1 Vida
+ */
 class BComun:public Bloque{
 public:
     BComun(){
@@ -65,6 +85,11 @@ public:
     }
 };
 
+/**
+ * Bloque Doble
+ * 15 Puntos
+ * 2 vidas
+ */
 class BDoble:public Bloque{
 public:
     BDoble(){
@@ -75,6 +100,11 @@ public:
     }
 };
 
+/**
+ * Bloque Triple
+ * 20 Puntos
+ * 3 vidas
+ */
 class BTriple:public Bloque{
 public:
     BTriple(){
@@ -85,6 +115,11 @@ public:
     }
 };
 
+/**
+ * Bloques internos
+ * 30 Puntos
+ * 1 Vida(Infinita si es no está en modo profundo)
+ */
 class BInterno:public Bloque{
 public:
     BInterno(){
@@ -95,6 +130,12 @@ public:
     }
 };
 
+
+/**
+ * Bloque Profundo
+ * 0 puntos
+ * Vida infinita
+ */
 class BProfundo:public Bloque{
 public:
     BProfundo(){
@@ -108,18 +149,40 @@ public:
     }
 };
 
+/**
+ * Matriz que contiene un grid de los bloques
+ * Contiene operaciones necesarias para el tablero
+ */
 class Bloques{
+
 private:
 Bloque *grid[6];//
+
 public:
+
     Bloque** getgrid(){
     return grid;
     }
 
+    /**
+     * Devuelve un bloque en específico de la matriz
+     * @param i (Posición fila)
+     * @param j (Posición columna)
+     * @return Bloque de la posición indicada
+     */
     Bloque getBlock(int i,int j){
     return this->grid[i][j];
     }
 
+    /**
+     * Llena el grid de bloques
+     * La probabilidad se ajusta de manera que solo puedan haber dos sets de bloques profundos por juego
+     * Probabilidades:
+     * Común=30%->40%->50% (después del ajuste de profundos)
+     * Doble=30%
+     * Triple=20%
+     * Profundo=20%->10%->0% (después del ajuste de profundos)
+     */
     void fillgrid(){
         int ajusteprofundos=0;
         int profundos[2];
@@ -127,24 +190,24 @@ public:
         for(int i=0;i<6;i++){
             Bloque row[13];
             for(int j=0;j<13;j++){
-                int probability=rand()%11+1+ajusteprofundos;
-                if(j==profundos[0]||j==profundos[1]){
+                int probability=rand()%11+1+ajusteprofundos;//Número del 1-10 con ajuste si ya se tienen profundos
+
+                if(j==profundos[0]||j==profundos[1]){//Bloque profundo, por lo que los de arriba deben ser internos
                     BInterno newblock;
                     row[j]=newblock;
-                }else if (probability<=2){
+                }else if (probability<=2){//Probabilidad Profundos 20%->10%->0%(ajustes)
                     BProfundo newBlock;
                     cantprofundos+=1;
                     profundos[cantprofundos]=j;
                     ajusteprofundos+=1;
                     row[j]=newBlock;
-                    cout<<"Yes"<<endl;
-                }else if(probability<=4){
+                }else if(probability<=4){//Probabilidad Triples 20%
                     BTriple newBlock;
                     row[j]=newBlock;
-                }else if(probability<=7){
+                }else if(probability<=7){//Probabilidad Dobles 30%
                     BDoble newBlock;
                     row[j]=newBlock;
-                }else{
+                }else{//Probabilidad comúnes 30%->40%->50%
                     BComun newBlock;
                     row[j]=newBlock;
                 }
@@ -154,9 +217,12 @@ public:
     }
 };
 
+
+
 class server{
 public:
     Bloques Grid;
+    int Ballspeeds[100];
     int Puntaje;
     int Bloques;
     int CantBolas;
@@ -168,6 +234,39 @@ public:
         this->CantBolas=1;
         this->Profundidad=0;
         this->Grid.fillgrid();
+        this->Ballspeeds[0]=5;
+    }
+    void LostBall(int index){
+        for(int j=index+1;j<this->CantBolas+1;j++){
+            this->Ballspeeds[index]=this->Ballspeeds[j];
+            index++;
+        }
+        this->CantBolas--;
+    }
+
+    void GainedBall(){
+        this->Ballspeeds[this->CantBolas]=5;
+        this->CantBolas++;
+    }
+
+    void SpeedSurprise(bool Buff){
+        if (Buff){
+            for(int i=0;i<this->CantBolas;i++){
+                if(this->Ballspeeds[i]!=10){
+                    this->Ballspeeds[i]++;
+                }else{
+                    continue;
+                }
+            }
+        }else{
+            for(int i=0;i<this->CantBolas;i++){
+                if(this->Ballspeeds[i]!=1){
+                    this->Ballspeeds[i]--;
+                }else{
+                    continue;
+                }
+            }
+        }
     }
 };
 
